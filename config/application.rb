b088710b18a10de481e707f6a44172a14dcd314b
Julie Mao <jmao27@gmail.com>
2013-02-26 15:25:14 -0800

    Add devise users.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Vouch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For heroku deployment
+    config.assets.initialize_on_precompile = false
   end
 end
