b6f7265f0f84098a3cfd89e373f54ad77e9b4adf
Julie Mao <jmao27@gmail.com>
2013-03-05 11:37:52 -0800

    Manually adding bootstrap_flash view helper, as it fails on heroku deployment.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,26 @@ module ApplicationHelper
   def facebook_login_banner
     link_to "Sign In with Facebook", user_omniauth_authorize_path(:facebook), class: "btn-auth btn-facebook large"
   end
+
+  # Supposed to come with twitter bootstrap
+  ALERT_TYPES = [:error, :info, :success, :warning]
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      next unless ALERT_TYPES.include?(type)
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+                           msg.html_safe, :class => "alert fade in alert-#{type}")
+        flash_messages << text if message
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
