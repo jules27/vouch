114ab0a92e4d326d86b045383dcabfa1f4464aee
Julie Mao <jmao27@gmail.com>
2013-03-05 12:50:03 -0800

    Update production environment file for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,18 @@ Vouch::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(
+                                  application.js application.css
+                                  bootstrap.js bootstrap.css
+                                  active_admin.js active_admin.js
+                                  auth-buttons.css
+                                  share_vouch_lists.js
+                                  businesses.js businesses.css
+                                  landing.js landing.css
+                                  users.js users.css
+                                  vouch_items.js vouch_items.css
+                                  vouch_lists.js vouch_lists.css
+                                )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
