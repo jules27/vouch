2193015c8e57855a672aa5a9c08f8509a4c07dea
Julie Mao <jmao27@gmail.com>
2013-03-06 18:47:08 -0800

    Add facebook pop up windows for sharing.

--- app/assets/javascripts/share_vouch_list.js
+++ app/assets/javascripts/share_vouch_list.js
@@ -13,10 +13,12 @@ $(function() {
     self = this;
     self.friends = ko.observableArray([]);
     self.newFriendName = ko.observable();
+
     self.removeFriend = function(friend) {
       self.friends.remove(friend);
       return;
     };
+
     self.addFriend = function() {
       var friend_fb_id, friend_name;
       friend_name = $("#typed_friend_name").val();
@@ -36,6 +38,43 @@ $(function() {
       self.newFriendName("");
       return;
     };
+    self.save = function() {
+      $(".friend-list-error").hide();
+      $(".friend-list-notice").hide();
+      $(".friend-list-success").hide();
+
+      // Create a list of fb id's
+      var dataToSave = [];
+      $.each(self.friends(), function(index, value) {
+        dataToSave.push(value.fb_id());
+      });
+
+      if (dataToSave.length == 0) {
+        $(".friend-list-error").html("Please add some friends before sharing.");
+        $(".friend-list-error").fadeIn("fast");
+        return;
+      }
+
+      // FB.ui({method: 'apprequests',
+      //   title: 'Check out my Vouches!',
+      //   message: 'I have shared my vouches with you. Check it out on ' + document.URL + '!',
+      //   to: dataToSave
+      // }, requestCallback);
+
+      FB.ui({
+        method: "send",
+        to: dataToSave,
+        link: document.URL,
+        description: "I have shared my Vouches with you. Check it out!"
+      }, requestCallback);
+    };
+
+    function requestCallback(response) {
+      if (response != null) {
+        $(".friend-list-success").html('You have successfully shared your list!<a class="close" data-dismiss="alert">&#215;</a>');
+        $(".friend-list-success").fadeIn("fast");
+      }
+    }
   };
 
   ko.applyBindings(new FriendList());
