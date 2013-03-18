dc3a560fb492eeb42a71514ff337993b672c8405
Julie Mao <jmao27@gmail.com>
2013-03-18 15:50:04 -0700

    Add view and controller actions to add a new business. Still need to hook up with Yelp API.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -16,4 +16,27 @@ class BusinessesController < ApplicationController
       end
     end
   end
+
+  def new_by_type
+    @business = Business.new
+    @business_type = BusinessType.find_by_name(params[:type])
+    render action: "new_by_type",
+           locals: {
+                     business_type: @business_type
+                   }
+  end
+
+  def create
+    @business = Business.new(params[:business])
+    @business_type = BusinessType.find(params[:business][:business_type_id])
+
+    if @business.save
+      flash[:notice] = "New #{@business_type.name} \"#{@business.name}\" was successfully created."
+      # flash.keep(:notice)
+      redirect_to vouch_lists_path
+    else
+      flash[:error]  = @business.errors.full_messages
+      redirect_to new_business_by_type_path(@business_type.name)
+    end
+  end
 end
