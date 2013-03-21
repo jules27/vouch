e98d5ecbb989f28a03c9aa61b7121045b9b07ebe
Julie Mao <jmao27@gmail.com>
2013-03-21 13:58:06 -0700

    Add search for tag name in wish items. Add a duplicate item check for primary vouch list, similar to the check for duplicate items in wish lists.

--- app/models/wish_item.rb
+++ app/models/wish_item.rb
@@ -5,12 +5,11 @@ class WishItem < ActiveRecord::Base
   has_many   :wish_taggings
   has_many   :tags, through: :wish_taggings
 
-  # TODO: add taggings
-
   attr_accessible :wish_list_id, :business_id, :user_id
 
   validates_presence_of :wish_list_id, :business_id
   validate :no_duplicate_items_in_list
+  validate :no_duplicate_items_in_vouch_list
 
   def tag_list
     return if tags.empty?
@@ -31,8 +30,16 @@ class WishItem < ActiveRecord::Base
 
   def no_duplicate_items_in_list
     if wish_list.wish_items.find_by_business_id(self.business_id).present?
-      result = wish_list.wish_items.find_by_business_id(self.business_id)
       errors.add(:wish_list_id, "already contains item \"#{self.business.name}\".")
+    end
+  end
+
+  # Similar to the duplicate items check for wish list, except look for
+  # duplicates in the user's primary vouch list.
+  def no_duplicate_items_in_vouch_list
+    vouch_list = wish_list.user.vouch_list_primary
+    if vouch_list.vouch_items.find_by_business_id(self.business_id).present?
+      errors.add(:vouch_list, "already contains item \"#{self.business.name}\".")
     end
   end
 end
